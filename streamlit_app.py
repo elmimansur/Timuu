dc651a8fdd5620cebfe51c4758a799102e114932
Elmira <92085729+elmimansur@users.noreply.github.com>
2023-09-18 21:54:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,24 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import streamlit as st
-from streamlit_option_menu import option_menu
-from apps import home, heatmap, upload  # import your app modules here
 
-st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
+client_id = 'YOUR_CLIENT_ID'
+client_secret = 'YOUR_CLIENT_SECRET'
 
-# A dictionary of apps in the format of {"App title": "App icon"}
-# More icons can be found here: https://icons.getbootstrap.com
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-apps = [
-    {"func": home.app, "title": "Home", "icon": "house"},
-    {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
-    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
-]
+st.title("Travel Through Time: A Musical Journey")
+year = st.slider("Select a year to explore music:", 1950, 2020)
+limit = st.slider("Number of tracks to fetch:", 10, 50)
 
-titles = [app["title"] for app in apps]
-titles_lower = [title.lower() for title in titles]
-icons = [app["icon"] for app in apps]
+def get_tracks_from_year(year, limit=50):
+    query = f'year:{year}'
+    results = sp.search(q=query, type='track', limit=limit)
+    tracks = results['tracks']['items']
+    return tracks
 
-params = st.experimental_get_query_params()
+tracks = get_tracks_from_year(year, limit)
 
-if "page" in params:
-    default_index = int(titles_lower.index(params["page"][0].lower()))
-else:
-    default_index = 0
-
-with st.sidebar:
-    selected = option_menu(
-        "Main Menu",
-        options=titles,
-        icons=icons,
-        menu_icon="cast",
-        default_index=default_index,
-    )
-
-    st.sidebar.title("About")
-    st.sidebar.info(
-        """
-        This web [app](https://share.streamlit.io/giswqs/streamlit-template) is maintained by [Qiusheng Wu](https://wetlands.io). You can follow me on social media:
-            [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu).
-        
-        Source code: <https://github.com/giswqs/streamlit-template>
-
-        More menu icons: <https://icons.getbootstrap.com>
-    """
-    )
-
-for app in apps:
-    if app["title"] == selected:
-        app["func"]()
-        break
+for track in tracks:
+    st.write(f"Track: {track['name']} by {track['artists'][0]['name']}")

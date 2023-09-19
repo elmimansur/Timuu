c9e2b56dc01b5ae2a989b24d86af1fece4fbf348
Elmira <92085729+elmimansur@users.noreply.github.com>
2023-09-19 01:29:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,6 @@ client_id = os.environ.get('YOUR_CLIENT_ID')
 client_secret = os.environ.get('YOUR_CLIENT_SECRET')
 
 
-client_id = 'YOUR_CLIENT_ID'
-client_secret = 'YOUR_CLIENT_SECRET'
-
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
